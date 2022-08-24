b8f1ee0cde66e647e892cb03a77b068749ace7bb
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-08-24 15:17:33 +0700

    writing test to check for thrown exceptions

--- TipCalculator/Models/TipCalculator.swift
+++ TipCalculator/Models/TipCalculator.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 
+enum TipCalculatorError: Error {
+    case invalidInput
+}
+
 class TipCalculator {
-    
-    func calculate(total: Double, percentage: Double) -> Double {
+     
+    func calculate(total: Double, percentage: Double) throws -> Double {
+        
+        if total < 0 {
+            throw TipCalculatorError.invalidInput
+        }
+        
         return total*percentage
     }
 }
